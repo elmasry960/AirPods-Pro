2c76d1a4039fc219256742df0f59d4a5d43b08ba
Eslam_Elmasry <eslamelmasry960@gmail.com>
2022-08-04 04:36:04 +0200

    update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const controller = new ScrollMagic.Controller();
 
 //Scenes
 let scene = new ScrollMagic.Scene({
-  duration: 10000,
+  duration: 43000,
   triggerElement: intro,
   triggerHook: 0
 })
@@ -30,7 +30,7 @@ let scene2 = new ScrollMagic.Scene({
   .addTo(controller);
 
 //Video Animation
-let accelamount = 0.1;
+let accelamount = 0.5;
 let scrollpos = 0;
 let delay = 0;
 
